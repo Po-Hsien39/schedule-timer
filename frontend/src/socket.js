5cc88cd6dcdef6191ea6a53a7e9ec3c86834b712
Tristan <wubaixian0725@gamil.com>
2024-05-27 13:37:03 +0800

    Prepare for deploying endpoint

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:5000';
+const URL = 'https://schedule-timer.onrender.com';
 
 export const socket = io(URL);
